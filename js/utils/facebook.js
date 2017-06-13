cfc64ed968c0d8353f79964ffada6d7271a3b8c4
Ruth Salvador <ruth.abby.sz@gmail.com>
2017-06-13 16:16:44 -0500

    fb funcion doLogin

--- js/utils/facebook.js
+++ js/utils/facebook.js
@@ -6,3 +6,21 @@ window.fbAsyncInit = () => {
     version    : 'v2.8' // usar la versión 2.8
   });
 };
+
+function loginHandler(response) {
+  if (response.status === 'connected') {
+    state.status = "Conectado";
+    FB.api('/me?fields=email,name', user => {
+      state.user = user;
+      state.doRender();
+    });
+  } else if (response.status === 'not_authorized') {
+    state.user = null;
+    state.status = "Aplicación no autorizada";
+    state.doRender();
+  }
+}
+
+function doLogin() {
+  FB.login(loginHandler, {scope: 'email'});
+}
